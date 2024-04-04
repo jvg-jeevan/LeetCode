8f763bdab7af189a67435a29f2297d28f1275d49
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-04 19:45:58 +0530

    Time: 44 ms (12.31%), Space: 16.5 MB (93.11%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,4 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         nums[:] = [x for x in nums if x!=val]
+        # nums[:] is used to modify the nums in place instead of creating the new list
+        # if the number x is not equal to the value specified then add that to nums in other words removing the val specified
         return len(nums)
+        # return the length of nums
