367603dc138cfc36a6e0d31bd0eefaaee5effb7f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-04 19:31:14 +0530

    Time: 1704 ms (19.53%), Space: 17.5 MB (64.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # iterate through the list elements
         for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
+                # if sum of two numbers in list is equal to target then return the index
                 if (nums[i] + nums[j]) == target:
                     return [i, j]
+        # if no such pair found return empty list
         return []
