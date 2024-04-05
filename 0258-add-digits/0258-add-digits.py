5cf3e7a6fb3307c92abefbc1432c8a91b1f91997
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-05 22:32:56 +0530

    Time: 37 ms (51.35%), Space: 16.6 MB (53.56%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        # conver the number to string and then list of integers
+        res = sum(list(map(int, str(num))))
+        # if the result is greater than 9 then call the function recursively
+        if res > 9:
+            return self.addDigits(res)
+        # else return the result
+        return res
