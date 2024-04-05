15886bef9a6a2958e21fb941fb0c426485bc63f9
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-05 22:35:06 +0530

    Time: 43 ms (14.92%), Space: 16.6 MB (11.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,5 +1,6 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        # using recursion
         # # conver the number to string and then list of integers
         # res = sum(list(map(int, str(num))))
         # # if the result is greater than 9 then call the function recursively
@@ -8,6 +9,8 @@ class Solution:
         # # else return the result
         # return res
 
+# if num is completely divisible by 9 then sum will be 9
+# else sum will be the remainder
         if num == 0:
             return 0
         if num % 9 == 0:
