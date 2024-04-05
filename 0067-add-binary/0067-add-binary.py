9a8888c69878edb43e2618edce6e06adf56ebbc5
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-05 07:35:50 +0530

    Time: 30 ms (92.45%), Space: 16.5 MB (70.3%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,8 +1,12 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a = a[::-1]
+        # reverse the string to get decimal of the binary number because string indexing start from right 0
         b = b[::-1]
+        # initialize numbers 
         num1, num2 = 0, 0
+
+        # convert to binary by multiplying each to 2 powers 
         for i in range(len(a)):
             rem = int(a[i]) * (2**i)
             num1 += rem
@@ -11,5 +15,6 @@ class Solution:
             rem = int(b[j]) * (2**j)
             num2 += rem
         
+        # add two numbers and convert to binary and return excluding '0b' part
         s1 = str(bin(num1 + num2))
         return s1[2:]
