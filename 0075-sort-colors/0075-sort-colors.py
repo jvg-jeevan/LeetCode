13a2859384e02a1e94fa23b78e317469c3e8240a
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-05 07:41:28 +0530

    Time: 29 ms (94.32%), Space: 16.4 MB (99.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,9 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # bubble sort technique
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] > nums[j]:
-                    # swap the numbers if pervious is greater than next
                     nums[i], nums[j] = nums[j], nums[i]
