6b77ef90e5c6dc83b1437277daaa3728e0efd053
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-06 22:28:53 +0530

    Time: 36 ms (48.66%), Space: 16.6 MB (62.29%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,22 +1,19 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
+# number = 0 steps = 0
         if num == 0:
             return 0
+# intitalize step
         step = 0
+# while num > 0
         while num:
+        # if num is odd then it needs 2 step i.e subtract 1 and divide by 2
             if num & 1:
                 step += 2
+        # if number is even then 1 step to divide by 2 
             else:
                 step += 1
+        # divide the number by 2 (right shift)
             num >>= 1
+# return step - 1 as num becomes 1 even though its odd it only takes 1 step
         return step - 1
-# class Solution:
-#     def numberOfSteps(self, num: int) -> int:
-#         step = 0
-#         while num:
-#             if num & 1:  # Check if num is odd
-#                 step += 2  # Increment step by 2 for the two operations
-#             else:
-#                 step += 1  # Increment step by 1 for division by 2
-#             num >>= 1  # Right shift num by 1 bit (equivalent to num //= 2)
-#         return step - 1  # Subtract 1 to account for the extra step when num becomes 0
