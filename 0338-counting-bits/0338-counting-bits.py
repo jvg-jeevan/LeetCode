0704f6788cb1ab5fcfc0923289e6faec65356ee5
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-06 06:32:41 +0530

    Time: 68 ms (43.82%), Space: 23.3 MB (48.88%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,4 +4,4 @@ class Solution:
 # for each number in range convert to binary and then to string and then count the number of 1s and append that to list
         for i in range(n+1):
             res.append(str(bin(i)).count('1'))
-        return out_list
+        return res
