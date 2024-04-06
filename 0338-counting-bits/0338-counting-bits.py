7c36c03647f5bc79331390f6a968f987e3b9c3ba
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-06 06:32:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+# for each number in range convert to binary and then to string and then count the number of 1s and append that to list
+        for i in range(n+1):
+            res.append(str(bin(i)).count('1'))
+        return out_list
