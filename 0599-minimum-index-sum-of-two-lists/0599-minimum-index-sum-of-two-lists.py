8ab96d6d2a6d4ee0fc7cadbdf451abc729f5d848
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-06 17:21:46 +0530

    Time: 243 ms (18.66%), Space: 17 MB (98.64%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -2,9 +2,6 @@ class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
 # get the elements in both the list
         out_list = [x for x in list1 if x in list2]
-# if only one element is common then return the element
-        # if len(out_list) == 1:
-        #     return out_list
 
 # dict1 is used to store the sum of indexes of common elements 
         dict1 = {}
