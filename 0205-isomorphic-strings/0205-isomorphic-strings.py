2b91820e052df5b7202a87c9b600516313dc51b5
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 23:02:04 +0530

    Time: 57 ms (5.19%), Space: 17 MB (19.38%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(set(sorted(s))) != len(set(sorted(t))):
+            return False
+        dict1 = {}
+        for i in range(len(t)):
+            dict1[t[i]] = s[i]
+
+        for x in range(len(s)):
+            if s[x] != dict1[t[x]]:
+                return False
+        return True
