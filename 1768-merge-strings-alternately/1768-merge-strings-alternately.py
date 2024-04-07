621a2a16e5e6567cb2642c22d2c6325ad5733195
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 10:13:56 +0530

    Time: 39 ms (28.15%), Space: 16.6 MB (12.38%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,10 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+# word3 contains result
         word3 = ''
+# this is to make the length of string equal
+# add the whitespaces to the word that has less characters than the other
+# ljust is used to add desired character at the end of string upto specified number of spaces
         if len(word1) <= len(word2):
             word1 = word1.ljust(max(len(word1), len(word2)))
         else:
             word2 = word2.ljust(max(len(word1), len(word2)))
+
+# for each character in word1 and word2 if char is whitespace then add only other words character of else add the characters alternately
         for i in range(len(word1)):
             if word1[i] == ' ':
                 word3 += word2[i]
