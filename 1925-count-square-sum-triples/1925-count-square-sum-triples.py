e2d728435ebd25479b04ebd3649aeaede5a1695e
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 10:47:36 +0530

    Time: 197 ms (51.7%), Space: 16.5 MB (89.42%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -1,9 +1,13 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         res = 0
+# a in range(n) and b in range(a)
         for a in range(1, n):
             for b in range(1, a):
+# if integer part of c is equal to c then its perfect sqrt and also c should be less than n
                 c = (a**2 + b**2) ** 0.5
+                # the condition
                 if int(c) == c and c <= n:
                     res += 2
+# res + 2 because of the combinations (3, 4, 5) and (4, 3, 5)
         return res
