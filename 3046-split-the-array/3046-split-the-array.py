8a9054c3a676ca2294daac402426269a9abd2d68
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 23:45:56 +0530

    Time: 60 ms (5.9%), Space: 16.5 MB (95.51%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,10 +1,12 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
-# # dict1 stores the occurrences of numbers, key= number, value= count
-#         dict1 = {}
-#         for i in set(nums):
-#             dict1[i] = nums.count(i)        
-# # to split into 2 lists of distinct elements  
-#         return all(x <= 2 for x in dict1.values())
-# # count of elements should be <= 2 all() checks the condition satisfies if all condition is True returns True or else False
-        return all(nums.count(i) <= 2 for i in set(nums))
+# dict1 stores the occurrences of numbers, key= number, value= count
+        dict1 = {}
+        for i in set(nums):
+            dict1[i] = nums.count(i)        
+# to split into 2 lists of distinct elements  
+        return all(x <= 2 for x in dict1.values())
+# count of elements should be <= 2 all() checks the condition satisfies if all condition is True returns True or else False
+
+#         return all(nums.count(i) <= 2 for i in set(nums))
+# # same solution in one line i.e the loop checks the condition that it has <= 2 counts and returns True or False accordingly
