9cd8ede8e1e812eab0754486952ac2112d28991a
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 23:07:52 +0530

    Time: 48 ms (23.4%), Space: 17 MB (17.1%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-# if length of
+# if length of s and t after removing duplicates are not same then False
         if len(set(sorted(s))) != len(set(sorted(t))):
             return False
+# dict1 is used to map the chars in s and t, key = t, val = s
         dict1 = {}
         for i in range(len(t)):
             dict1[t[i]] = s[i]
 
+# for every char in s if the mapped value in dict1 does not match then return False 
         for x in range(len(s)):
             if s[x] != dict1[t[x]]:
                 return False
+# if all the characters match in mapping then return True
         return True
