091c933394e1cb4d98b17afc88d80ba882eafc7f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 22:39:36 +0530

    Time: 29 ms (99.43%), Space: 17 MB (15.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+# the index at which the array is rotated i.e the elements previous to that is greater
         if target in nums:
+            # if target found return index else return -1
             return nums.index(target)
         return -1
