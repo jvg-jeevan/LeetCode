eefe0a532c8409e0741cf8cdd9435221bc70f25b
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 10:38:54 +0530

    Time: 64 ms (18.2%), Space: 16.7 MB (93.11%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+# intitalize the product to 1 
+        product = 1
+# for each element in list find the product, if any element in list is zero then the product becomes zero
+        for i in nums:
+            if i == 0:
+                product = 0
+                break
+            product *= i
+
+# return result as per condition
+        if product > 0:
+            return 1
+        elif product < 0:
+            return -1
+        else:
+            return 0
+            
