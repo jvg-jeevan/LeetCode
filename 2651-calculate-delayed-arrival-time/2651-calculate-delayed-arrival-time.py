4bd619c9716b4d19a7e55e6aa5007d4118923326
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 16:06:58 +0530

    Time: 32 ms (72.81%), Space: 16.6 MB (10.42%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
@@ -0,0 +1,4 @@
+class Solution:
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+        return (arrivalTime + delayedTime) % 24
+# add arrival and delayed time and return the remainder obtained from the sum
