9f3b0feb6db5ec33d61b971daf52e6128171d157
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-07 10:03:49 +0530

    Time: 55 ms (46.87%), Space: 16.5 MB (92.09%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -2,9 +2,14 @@ class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         dict1 = {}
         s2 = ''
+# dict1 mapping of each letter to index , key= index, value= character
         for i in range(len(s)):
             dict1[indices[i]] = s[i]
+# sort the keys in dict1 and it returns list of sorted keys dict2
         dict2 = sorted(dict1)
+
+# for each key index in dict2 get the value for key in dict1
         for i in dict2:
             s2 += dict1[i]
-        return s2 
+# append the value to the string
+        return s2
