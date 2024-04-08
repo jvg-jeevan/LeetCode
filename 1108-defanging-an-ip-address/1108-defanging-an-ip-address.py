7821093a2d415ba36c7316eb0993892e67bcd532
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-08 21:49:58 +0530

    Time: 26 ms (95.62%), Space: 16.4 MB (98.65%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,4 +1,13 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        return address.replace('.', '[.]')
-# replace(old, new) is used to replace all the occurrences of the old to new 
+#         return address.replace('.', '[.]')
+# # replace(old, new) is used to replace all the occurrences of the old to new 
+
+        result = ''
+        for char in address:
+            if char == '.':
+                result += '[.]'
+            else:
+                result += char
+        return result
+# create a variable result to store the result string iterate through the string address and if char is '.' then add '[.]' to result or else add the char
