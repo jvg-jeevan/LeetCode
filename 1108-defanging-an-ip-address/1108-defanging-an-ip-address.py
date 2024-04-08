b4b467cb31e199758a5a7c04b90bdba1c0db3e44
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-08 21:48:13 +0530

    Time: 35 ms (52.34%), Space: 16.4 MB (79.33%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,3 +1,4 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return address.replace('.', '[.]')
+# replace(old, new) is used to replace all the occurrences of the old to new 
