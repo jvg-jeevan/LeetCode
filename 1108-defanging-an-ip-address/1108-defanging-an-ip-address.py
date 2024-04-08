7c4ad890d0fbdb0132493f626910de347acc59ea
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-08 21:53:30 +0530

    Time: 39 ms (23.82%), Space: 16.8 MB (5.74%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -4,16 +4,17 @@ class Solution:
 #         return address.replace('.', '[.]')
 # # replace(old, new) is used to replace all the occurrences of the old to new 
 
-# approach 2
-        # result = ''
-        # for char in address:
-        #     if char == '.':
-        #         result += '[.]'
-        #     else:
-        #         result += char
-        # return result
-# create a variable result to store the result string iterate through the string address and if char is '.' then add '[.]' to result or else add the char
+# # approach 2
+#         return '[.]'.join(address.split('.'))
+# # split() the string to list at separator '.' then join the list elements with [.]
 
 # approach 3
-        return '[.]'.join(address.split('.'))
-# split() the string to list at separator '.' then join the list elements with [.]
+        result = ''
+        for char in address:
+            if char == '.':
+                result += '[.]'
+            else:
+                result += char
+        return result
+# create a variable result to store the result string iterate through the string address and if char is '.' then add '[.]' to result or else add the char
+
