b54b64941691df9ab4182127a8d5ca85a0984445
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-09 22:51:08 +0530

    Time: 32 ms (77.35%), Space: 16.6 MB (41.88%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.py
+++ 0553-optimal-division/0553-optimal-division.py
@@ -1,8 +1,11 @@
 class Solution:
     def optimalDivision(self, nums: List[int]) -> str:
+    # if nums length=1 then return str of num
         if len(nums) == 1:
             return str(nums[0])
+    # if len=2 then return str of a / b
         if len(nums) == 2:
             return str(nums[0]) + '/' + str(nums[1])
+    # if len is greater than 2 then a/(b/c/d.......)
         return str(nums[0]) + '/(' + '/'.join(list(map(str, nums[1:]))) + ')'
-        
+# here joining the string part of 1st number and then concatenating () and joining / between the numbers
