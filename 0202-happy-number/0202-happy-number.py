cffc87cfdb982b44b422f94a606cdf8d2dc32a82
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-11 23:22:13 +0530

    Time: 36 ms (63.17%), Space: 16.5 MB (81.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+    # visit is a set to store visited numeber
+        visit = set()
+    # while n not equal to 1
+        while n != 1:
+            # if number is already in visit then number is False
+            if n in visit:
+                return False
+            # if n not in visit add n to set(visit)
+            visit.add(n)
+        # to get the sum of squares of digits and assign the same to n
+            n = sum(int(i)**2 for i in str(n))
+    # if no elements in visited are repeated then True (until n becomes 1)
+        return True
