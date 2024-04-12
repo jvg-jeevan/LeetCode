f811efe9d663634a36fe85b5fec3c8ac211ede9d
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-13 00:05:45 +0530

    Time: 74 ms (87.32%), Space: 16.7 MB (34.11%) - LeetHub

--- 0372-super-pow/0372-super-pow.py
+++ 0372-super-pow/0372-super-pow.py
@@ -1,3 +1,4 @@
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
+        # convert b from list to num by mapping each digit to str and then joining and converting back to int
         return pow(a ,int(''.join(map(str, b))), 1337)
