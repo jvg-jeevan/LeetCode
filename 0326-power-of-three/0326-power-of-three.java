487d0f7dd641cbb6384de3a52a01c420c1905a16
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-14 22:02:19 +0530

    Time: 8 ms (90.09%), Space: 44.3 MB (17.96%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
+        // take while loop for n > 0
         while(n>0){
+// if n becomes 1 then n completely divisible by 3 i.e the number is pow (3)
             if (n == 1){
                 return true;
             }
+    // if n is not divisible by 3 then return break and return False
             else if (n%3 != 0){
                 break;
             } 
+    // divide n by 3 and assign it to n
             n /= 3;
         }
         return false;
