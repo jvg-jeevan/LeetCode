e97cd69ea2882f3b5d0c859c991d369bd9c2a820
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-15 22:18:16 +0530

    Time: 0 ms (100%), Space: 40.3 MB (75.66%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,13 @@
 class Solution {
     public int hammingWeight(int n) {
         int res = 0;
+        // while n is not zero 
         while(n!=0){
+            // if remainder is not zero then increment res
             if (n%2 != 0)
             res += 1;
-            n = n / 2; 
+            // divide the number n by 2
+            n = n >> 1; 
         }
         return res;
     }
