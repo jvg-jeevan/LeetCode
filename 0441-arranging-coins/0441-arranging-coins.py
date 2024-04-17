fbe3219da18a59b83470a42a044d3c207ca74931
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-17 18:53:12 +0530

    Time: 406 ms (43.27%), Space: 16.6 MB (40.41%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,7 +1,12 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+# i= number of coins placed
         i = 1
+# arranging the coins from 1 
         while n >= i:
             n -= i
             i += 1
         return i - 1
+# while number of coins is greater than the number of coins placed
+# decrement n, i.e n will be remaining coins
+# increment i, i.e wiil be the rows placed
