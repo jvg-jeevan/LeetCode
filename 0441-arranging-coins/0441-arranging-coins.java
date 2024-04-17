805d6f0d2522a974a4fc24b20706a0ca06d8ee33
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-17 18:53:53 +0530

    Time: 5 ms (50.99%), Space: 40.7 MB (65.83%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -8,7 +8,7 @@ class Solution {
             // increment the rows
             i += 1;
         }
-        // return the rows upto which the coins are placed
+// return the rows upto which the coins are placed (i-1) because if the coins are not completely placed
         return i-1;
     }
 }
