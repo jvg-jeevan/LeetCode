22944b77ffeb81956c3f4a2e47294cc7068af664
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-22 16:21:23 +0530

    Time: 7673 ms (10.8%), Space: 16.9 MB (5.19%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,22 +1,26 @@
 class Solution:
+# get the factorial using recursion
     def factorial(self, n):
         return n * factorial(n-1)
+    
     def trailingZeroes(self, n: int) -> int:
-        # fact = 1
-        # for i in range(1, n+1):
-        #     fact *= i
+# positive number then get factorial
         if n > 0:
             fact = self.factorial(n)
         else:
             fact = 1
-
+# result to store the count of trailing zeros
         result = 0
-        if fact % 5 != 0:
+# if last number is not divisible by 10 then no trailing zeros so return 0
+        if fact % 10 != 0:
             return result
+    # if fact > 9 then check for last zero until > 9
         while fact > 9:
+# if last digit is zero then increment result and divide fact bt 10
             if fact % 10 == 0:
                 result += 1
                 fact//= 10
+# if not divisible by 10 then break
             else:
                 break
         return result
