e2b4e83ff523824fc304082f89f24db0de706a2d
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-22 16:45:17 +0530

    Time: 29 ms (96.5%), Space: 16.7 MB (19.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # to get the transpose of the matrix (takes 1 element from each row)
+# to get the transpose of the matrix (takes 1 element from each row)
         matrix[:] = [list(row) for row in zip(*matrix)]
-        # for each row in the matrix reverse the elements in the row
+# for each row in matrix reverse the elements in the row alter in same
         for row in matrix:
             # row.reverse()
             row[:] = row[::-1]
