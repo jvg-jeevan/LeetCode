9c29639f01bb50241874ff0745bfe17a76e77a38
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-22 19:23:11 +0530

    Time: 131 ms (7.26%), Space: 23.2 MB (84.95%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,7 +1,24 @@
 class Solution:
+    def count_ones(self, num):
+        """This function gets the number of ones in the binary representation of a number"""
+        ones = 0
+        while num != 0:
+            if num % 2 != 0:
+                ones += 1
+            num //= 2
+        return ones
+
     def countBits(self, n: int) -> List[int]:
-        res = []
-# for each number in range convert to binary and then to string and then count the number of 1s and append that to list
-        for i in range(n+1):
-            res.append(str(bin(i)).count('1'))
-        return res
+        """This function gets the number of ones by calling the count_ones() function in the range of n"""
+        return [self.count_ones(i) for i in range(n+1)]
+
+
+
+
+
+
+#         res = []
+# # for each number in range convert to binary and then to string and then count the number of 1s and append that to list
+#         for i in range(n+1):
+#             res.append(str(bin(i)).count('1'))
+#         return res
