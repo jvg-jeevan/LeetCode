62e862ce525e27174aad8cb3ccaf71d4b2c2fefc
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-23 22:34:49 +0530

    Time: 695 ms (98.75%), Space: 32.3 MB (28.47%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,11 +1,14 @@
 class Solution:
     def maximumGap(self, nums: list[int]) -> int:
+# sort the nums list and remove the duplicate elements by converting to set
         nums = sorted(set(nums))
+# initialize the difference as 0 
         res_diff = 0
-        
+# if length of nums is less than 2 the no difference
         if len(nums) <= 1:
             return res_diff
         
+# iterate through each element and find the difference and max of both and assign to the res_diff and then return the result
         for i in range(len(nums)-1):
             res_diff = max(res_diff, (nums[i+1] - nums[i]))
         
