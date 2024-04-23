e79d88cf11c653a8b3b27334cbddb6b0afc58362
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-23 22:21:23 +0530

    Time: 40 ms (91.26%), Space: 16.8 MB (37.09%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,3 +1,4 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+# as the largest element will be greater than the neighbors so return the index
         return nums.index(max(nums))
