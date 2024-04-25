8feef0d7ec732bb6b6fa5e4d4c37f470fa5f67d6
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-25 15:57:43 +0530

    Time: 113 ms (18.61%), Space: 17.5 MB (25.3%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,20 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)-1):
-            for j in range(i+1, len(numbers)):
-                if (numbers[i] + numbers[j]) == target:
-                    return [i+1, j+1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+# binary search approach
+# initialize left and right pointers
+        left = 0
+        right = len(nums) - 1
+
+# continue until you get the equal
+        while left < right:
+# get the sum
+            total = nums[left] + nums[right]
+# if total equals to sum then return the index
+            if total == target:
+                return [left + 1, right + 1]
+# if total is greater than target then decrease right pointer (move towards smaller value)
+            elif total > target:
+                right -= 1
+# if total is less than target then increase left pointer (move towards greater value)
+            else:
+                left += 1
