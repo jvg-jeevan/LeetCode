3fe36181c92d044d704ee08129bea6ff6d1c46f1
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-28 21:51:42 +0530

    Time: 37 ms (95.87%), Space: 16.7 MB (11.16%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.py
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.py
@@ -2,11 +2,12 @@ class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
 # sort the list nums
         nums.sort()
-# instead of removing the elements, the number will be after index of k i.e  index number of steps to get k
-        if k in nums:
-            return nums.index(k)
-        res = 0
-        for i in range(len(nums)-1):
-            if k > nums[i+1] and k < nums[i]:
-                res = i
-        return res
+# initialize result
+        result = 0
+# for each element if less than k increment result, if element is greater break out of loop
+        for i in nums:
+            if i < k:
+                result += 1
+            elif i >= k:
+                break
+        return result
