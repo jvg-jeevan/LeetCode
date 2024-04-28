05d2794983d7ad9730a5c7490f4f300075f431cd
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-28 20:14:56 +0530

    Time: 33 ms (68.66%), Space: 16.6 MB (68.36%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -1,7 +1,9 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        result = 0
-        for i in range(low, high+1):
-            if i % 2 != 0:
-                result += 1
+# result contains even or odd numbres
+        result = (high - low) // 2
+# if any one of low or high is odd add 1 to result
+        if (low % 2 != 0) or (high % 2 != 0):
+            result += 1
+            
         return result
