0711fc8cba8a1a2685344219698a8f0ae5e40b18
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-29 18:44:32 +0530

    Time: 39 ms (40.54%), Space: 16.6 MB (69.24%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+# if the concatenation of both string is not equal then return ' '
+        if (str1 + str2) != (str2 + str1):
+            return ''
+# get the length of both the strings
+        s1 = len(str1)
+        s2 = len(str2)
+
+# finding the gcd of the length using Euclidean algorithm
+        while s2 != 0:
+            s1, s2 = s2, s1 % s2
+# return the substring of str2 upto length of gcd
+        return str2[:s1]
