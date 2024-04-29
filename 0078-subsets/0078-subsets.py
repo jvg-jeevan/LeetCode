659e1f7252ef87bfcb2c346f5491b68d23e55e64
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-29 18:57:51 +0530

    Time: 34 ms (77.68%), Space: 16.7 MB (77.91%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,7 +3,6 @@ class Solution:
     def subsets(self, nums: list[int]) -> list[list[int]]:
 # itertools.combinations(iterable, n) returns n length subsequences of elements from the input iterable.
         result = []
-# result to store the lists
         for n in range(len(nums)+1):
 # to get subsets of all lengths
             result.extend(list(itertools.combinations(nums, n)))
