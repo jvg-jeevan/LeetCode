7c70bee18a51fdaa710b9c9bb0ecbaa099bc704c
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-04-30 22:06:09 +0530

    Time: 49 ms (12.85%), Space: 16.5 MB (72.22%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,7 +1,15 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        sumof = 0
-        for i in range(1, (num//2)+1):
+        if num == 1:
+# returns False because 1 is not considered a perfect number
+            return False
+# used to accumulate the sum of the divisors of num
+        sumof = 1
+# loop from 2 to square root of num
+        for i in range(2, int(num ** 0.5)+1):
+# if a i divides number completely
             if num % i == 0:
-                sumof += i
+# add the sum of divisors is updated by adding both i and num // i to sumof. This effectively adds the pair of divisors (i, num//i) to the sum
+                sumof += (i + num//i)
+# return if obtained sum is equal to num
         return sumof == num
