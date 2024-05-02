cb854f166e1c7daa69c52b0f68407479fdf06f81
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-02 09:20:05 +0530

    Time: 42 ms (22.96%), Space: 16.6 MB (9.6%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,28 +1,18 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-# initialize res to store the count 
+# create two sets for storing small and caps letters
+        small = set()
+        caps = set()
+# iterate through each char to find and add to caps or small
+        for char in word:
+            if char.islower():
+                small.add(char)
+            else:
+                caps.add(char)
+# initialize res to store the result count
         res = 0
-# convert the str word to char set and iterate through each char
-        for char in set(word):
-# check if both upper and lower exist and increment res
-            if (char.upper() in word) and (char.lower() in word):
+# for each char in caps convert to lower and check of existance if True increment res
+        for i in caps:
+            if i.lower() in small:
                 res += 1
-# return res/2 because it counts for caps and small twice
-        return res // 2
-
-# # create two sets for storing small and caps letters
-#         small = set()
-#         caps = set()
-# # iterate through each char to find and add to caps or small
-#         for char in word:
-#             if char.islower():
-#                 small.add(char)
-#             else:
-#                 caps.add(char)
-# # initialize res to store the result count
-#         res = 0
-# # for each char in caps convert to lower and check of existance if True increment res
-#         for i in caps:
-#             if i.lower() in small:
-#                 res += 1
-#         return res
+        return res
