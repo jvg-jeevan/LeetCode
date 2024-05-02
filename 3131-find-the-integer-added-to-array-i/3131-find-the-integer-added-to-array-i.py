49c870e513b22b235aa09e75200912ba7d9bf614
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-02 06:35:51 +0530

    Time: 53 ms (34.32%), Space: 16.6 MB (77%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
@@ -1,9 +1,11 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+# approach 1
 #         nums1.sort()
 #         nums2.sort()
 # # sort the elements in the list and get difference of 1st elements
 #         return nums2[0] - nums1[0]
 
+# approach 2
         return min(nums2) - min(nums1)
 # as the integer to be added will be same so get the difference of the minimum elements
