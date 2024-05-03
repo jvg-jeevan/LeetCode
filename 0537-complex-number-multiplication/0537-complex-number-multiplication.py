51f5c0942da02ee8223ffcb91fb6cabbe0a744f2
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-03 22:39:14 +0530

    Time: 35 ms (52.39%), Space: 16.4 MB (89.3%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.py
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.py
@@ -4,8 +4,9 @@ class Solution:
         num1 = num1.split('+')
         num2 = num2.split("+")
 
-# get the a, c real part and b, d imaginary part
+# get the a, c real part 
         a, c = int(num1[0]), int(num2[0])
+# b, d imaginary part excluding i
         b, d = int(num1[1][:-1]), int(num2[1][:-1])
 
 # formula for multiplication (a+ib) * (c+id) = (ac - bd) + (ad + bc)i
