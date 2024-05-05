d777a6978988156d9b6032769e8e149a2d969a92
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-05 23:38:23 +0530

    Time: 536 ms (41.23%), Space: 0B (100%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,6 +1,6 @@
--- using case similar to if else in update
+-- using case similar to if else or switch in update
 UPDATE Salary SET sex = 
 CASE SEX 
     WHEN 'f' THEN 'm'
-    ELSE 'f'
+    WHEN 'm' THEN 'f'
 END;
