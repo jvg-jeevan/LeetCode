75eed2ac8591d1a9bbc60346975b0cdc657c451b
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-06 19:59:50 +0530

    Time: 1612 ms (50.31%), Space: 0B (100%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select u.name, sum(t.amount) as balance
+select u.name as name, sum(t.amount) as balance
 from Users u
 join Transactions t on u.account = t.account
 group by u.name
