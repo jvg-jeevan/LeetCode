f876bbc7a47aa729df6e6627eef181cc76852fbc
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-07 22:24:35 +0530

    Time: 1665 ms (28.5%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,6 +3,7 @@ from Products p
 join Orders o on p.product_id = o.product_id
 -- to select values between dates
 where o.order_date between '2020-02-01' and '2020-02-29'
+-- group based on product id
 group by o.product_id
 -- select units >= 100
 having sum(o.unit) >= 100;
