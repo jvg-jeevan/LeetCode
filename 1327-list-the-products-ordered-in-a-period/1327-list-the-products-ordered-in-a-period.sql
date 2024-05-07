dc2223646a2b41d18e640771061b76fbd9148548
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-07 22:24:50 +0530

    Time: 1050 ms (99.17%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,7 @@
 select p.product_name, sum(o.unit) as unit
 from Products p
 join Orders o on p.product_id = o.product_id
--- to select values between dates
+-- to select values between dates in febraury
 where o.order_date between '2020-02-01' and '2020-02-29'
 -- group based on product id
 group by o.product_id
