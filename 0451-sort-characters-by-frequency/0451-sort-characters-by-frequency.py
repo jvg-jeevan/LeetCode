a55b205d1485c778e7343479f2f0094a8774b100
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-10 23:27:31 +0530

    Time: 64 ms (15.47%), Space: 18.5 MB (25.32%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,13 +2,18 @@ from collections import Counter
 
 class Solution:
     def frequencySort(self, s: str) -> str:
+# sorted of the string input s results in sorted list of chars
         res = sorted(s)
+# Counter() creates the dict(), key= char, value= number of occurrences
         res = Counter(res)
-        ans = ''
 
+# ans to store the result to return
+        ans = ''
+# sort the dict based on values
         res = dict(sorted(res.items(), key= lambda items: items[1], reverse= True))
 
+# iterate throught each item in dict add the number of chars to ans
         for key, val in res.items():
             ans += val * key
-            
+# return the result ans
         return ans
