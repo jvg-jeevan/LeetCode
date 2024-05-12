112c7335b8c6dfc1d24f2ba51c6357c57ba046e3
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-12 22:28:42 +0530

    Time: 53 ms (40.46%), Space: 16.8 MB (10.63%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.py
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.py
@@ -1,9 +1,13 @@
 from collections import Counter
 class Solution:
     def maximumNumberOfStringPairs(self, words: list[str]) -> int:
+# res dict() key = tuple(sorted elements(list)) and value = occurrences (pairs)
         res = Counter(tuple(sorted(each)) for each in words)
+# ans to store the occurrences
         ans = 0
+# check if pairs increment pairs
         for key, val in res.items():
             if val == 2:
                 ans += 1
+# return the number of pairs
         return ans
