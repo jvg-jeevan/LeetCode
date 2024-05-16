c637c21e11437fdee02c86df0ae690ea6ab0bf23
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-16 22:50:09 +0530

    Time: 75 ms (17.52%), Space: 16.8 MB (93.07%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -6,7 +6,7 @@ class Solution:
         res = 0
 # iterate through each word in words from left to right inclusive
         for word in words[left: right+1]:
-# if 1st and last char in word is in vowels list increment res
+# if 1st and last char in word is in vowels list then increment res
             if (word[0] in vowels) and (word[-1] in vowels):
                 res += 1
 # return the res count
