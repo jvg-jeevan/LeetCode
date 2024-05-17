b0b5978c674d89eee27152b2987ae9396e5dff5f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-17 12:11:02 +0530

    Time: 532 ms (98.28%), Space: 34.8 MB (21.26%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.py
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.py
@@ -1,25 +1,39 @@
 class Solution:
     def matrixSum(self, nums: list[list[int]]) -> int:
 
-# approach 1 taking indivisual element 
-# res to store the resultant sum
-        res = 0
+# # approach 1 taking indivisual element 
+# # res to store the resultant sum
+#         res = 0
         
-# run until the first row is empty
-        while nums[0]:
-# matrix to store the maximum elements in each row
-            matrix = []
-# for each row in nums
-            for row in nums:
-# maxi store maximum element
-                maxi = max(row)
-# remove the maximum element form row
-                row.remove(maxi)
-# append the element maxi to matrix
-                matrix.append(maxi)
+# # run until the first row is empty
+#         while nums[0]:
+# # matrix to store the maximum elements in each row
+#             matrix = []
+# # for each row in nums
+#             for row in nums:
+# # maxi store maximum element
+#                 maxi = max(row)
+# # remove the maximum element form row
+#                 row.remove(maxi)
+# # append the element maxi to matrix
+#                 matrix.append(maxi)
 
-# get the maximum element in matrix and update the res
-            res += max(matrix)
+# # get the maximum element in matrix and update the res
+#             res += max(matrix)
 
+# # return res
+#         return res  
+
+
+# appraoch 2 using zip() function
+
+        res = 0
+# sort() each row in reverse order
+        for row in nums:
+            row.sort(reverse= True)
+
+# zip(*nums) takes 1 element from each column and finds the maximum element from each column and add that to res
+        for row in zip(*nums):
+            res += max(row)
 # return res
-        return res  
+        return res
