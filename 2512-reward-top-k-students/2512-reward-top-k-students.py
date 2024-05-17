5ada9b08df3a694d3659c64b6f01a8341ae3dfce
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-17 17:53:43 +0530

    Time: 338 ms (10.3%), Space: 32.4 MB (11.42%) - LeetHub

--- 2512-reward-top-k-students/2512-reward-top-k-students.py
+++ 2512-reward-top-k-students/2512-reward-top-k-students.py
@@ -1,28 +1,35 @@
 class Solution:
     def topStudents(self, positive_feedback: list[str], negative_feedback: list[str], report: list[str], student_id: list[int], k: int) -> list[int]: 
 
+# remove the duplicates from positive and negative_feedback
         positive_feedback = set(positive_feedback)
         negative_feedback = set(negative_feedback)
 
+# res dict() key= student_id, value= mark(based on pos and neg feedbacks)
         res = {}
         
+# feedback stores list of each string
         feedback = [each.split() for each in report]
 
         print(feedback)
 
+# for each feedback and student_id
         for feed, stud in zip(feedback, student_id):
+# mark to count the number of positive and negaticve feedbaks
             mark = 0
             for val in feed:
+# for each val in feedback of indivisual count the number of pos and neg feedback
                 if val in positive_feedback:
                     mark += 3
                 elif val in negative_feedback:
                     mark -= 1
 
+# res dict() for every student mark of pos and neg
             res[stud] = mark
 
-        res = sorted(res.items(), key=lambda item: (-item[1], item[0]))
+# sort the dict based on increasing order of student id and decreasing order of mark(pos - neg)
+        res = dict(sorted(res.items(), key=lambda item: (-item[1], item[0])))
+        print(res)
         
-        # Extract the top k student IDs
-        return [student[0] for student in res[:k]]
-        
-        # return top_students
+# return the top k students
+        return list(res.keys())[:k]
