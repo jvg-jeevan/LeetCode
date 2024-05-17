7b75b6e95c7a48248491b3afef5e88829624bd7b
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-17 12:25:11 +0530

    Time: 49 ms (40.45%), Space: 16.6 MB (14.64%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+# res to store the number of passangers
+        res = 0
+# for each passanger
+        for num in details:
+# num[11:13] gives the age of passanger is strictly greater than 60 increment res
+            if int(num[11:13]) > 60:
+                res += 1
+# return the number of passangers
+        return res
