7daaad317ae1c0e973ee228693d530f14f4d498b
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-19 17:39:03 +0530

    Time: 730 ms (82.79%), Space: 17.1 MB (68.62%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rowAndMaximumOnes(self, mat: list[list[int]]) -> list[int]:
+# nums list to store the count of 1s
+        nums = []
+# for each row in mat, append the number of 1s in row
+        for row in mat:
+            nums.append(row.count(1))
+
+        maxi = max(nums)
+        return [nums.index(maxi), maxi]
+# # take 0th element as maxi
+#         maxi = nums[0]
+# # iterate through each element from 1st element
+#         for i in range(1, len(nums)):
+# # if element is greater than maxi then return [ind, element]
+#             if nums[i] > maxi:
+#                 return [i, nums[i]] 
+# # if no greater element is found then return 1st element
+#         return [0, maxi]
