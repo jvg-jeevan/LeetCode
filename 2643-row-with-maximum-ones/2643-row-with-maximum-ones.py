a29b34c533ecb4d8e74fb399eee6066134a22438
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-19 17:39:43 +0530

    Time: 746 ms (45.33%), Space: 17 MB (68.62%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -5,7 +5,7 @@ class Solution:
 # for each row in mat, append the number of 1s in row
         for row in mat:
             nums.append(row.count(1))
-
+# get the maximum element and find the index and return
         maxi = max(nums)
         return [nums.index(maxi), maxi]
 # # take 0th element as maxi
