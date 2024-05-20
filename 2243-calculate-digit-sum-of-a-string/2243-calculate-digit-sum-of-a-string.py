dd1c52629a1e58d0de108f4f47529367e9038071
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-20 16:36:10 +0530

    Time: 34 ms (66.67%), Space: 16.5 MB (41.09%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
@@ -1,14 +1,19 @@
 class Solution:
     def digitSum(self, string: str, k: int) -> str:
 
+# continue the execution only when len(string) > k
         while len(string) > k:
-
+# map() each char to int and convert that to list
             result = list(map(int, string))
 
+# split the list into nested list of desired size (in intervals of k)
             result = [result[i:i+k] for i in range(0, len(result), k)]
 
+# get the sum of each nested list and map() to str
             result = list(map(str, [sum(row) for row in result]))
 
+# join() every element from result and assign to string
             string = ''.join(result)
 
+# reutrn the resultant string
         return string
