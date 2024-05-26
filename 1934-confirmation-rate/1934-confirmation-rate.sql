5c4293f885a7fa0bebf6e8a9384bf690f35996cf
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-26 22:44:47 +0530

    Time: 1360 ms (46.14%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,3 +1,6 @@
+-- sum(case) i.e like if confirmed 1 else 0 take sum of 1's and get the average for each user_id 
+-- left join to include details of non confirmed details also
+
 SELECT 
     u.user_id, 
     ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*), 2) AS confirmation_rate
