011231497e84fe4e4096670cc6088bea9601e7a8
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-26 23:02:04 +0530

    Time: 1355 ms (53.1%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -5,5 +5,4 @@ where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
 
 -- ^[a-zA-Z] start with letter
 -- prefix [a-zA-Z0-9_.-]* containg letters or char or num from this
--- [.] to match .
--- @leetcode[.]com$ ending of string '$' to check the domain 
+-- @leetcode[.]com$ ending of string '$' to check the domain [.] to match .
