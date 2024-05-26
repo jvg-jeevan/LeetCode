e24c7d874f0deb40584c7232ecbfddaf023e7773
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-26 10:36:21 +0530

    Time: 72 ms (6.34%), Space: 16.6 MB (88.73%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,9 +1,9 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        # sets method
-# source will contain City A
+# sets difference method
+# set source will contain City A
         source = set(path[0] for path in paths)
-# destination will contain city B
+# set destination will contain city B
         destination = set(path[1] for path in paths)
 # the difference in destination and source will produce a city that is not in city A but only in city B and convert to list and return the element of list using pop() which returns the last and only element of list
         return list(destination - source).pop()
