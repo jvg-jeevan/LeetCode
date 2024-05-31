0089f1d0acaf5a93964e72e0c9097f6a079cb22f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-31 07:20:05 +0530

    Time: 38 ms (47.72%), Space: 16.5 MB (29.71%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,24 +1,30 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        num = 0
+# convert the string s to list of type int and reverse the list
         s = list(map(int, s))[::-1]
-        print(s)
-
-        for ind, (val) in enumerate(s):
+# initialize the num to 0
+        num = 0
+# converting the binary format to int i.e for every binary digit 
+        for ind, val in enumerate(s):
+# num += val times two power of index
             num += (val * (2 ** ind))
-        print(num)
 
-         
+# initialize res to zero   
         res = 0
+# while num > 1 continue inside while loop
         while num > 1:
+# if num becomes 1 break out of while and return the res
             if num == 1:
                 break
+# if num is even then reduce the num >> divides the num by two power 1
             elif num % 2 == 0:
                 num >>= 1
+# increment the res count to 1
                 res += 1
+# if num is odd then increment the num by 1 and also the res with 1
             else:
                 num += 1
                 res += 1
             
-            
+# return the res 
         return res
