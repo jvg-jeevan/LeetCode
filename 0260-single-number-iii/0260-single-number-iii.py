b071ac6e43e7f6e7686646c46e309c7dd3df2353
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-05-31 07:29:58 +0530

    Time: 65 ms (24.18%), Space: 18.7 MB (30.48%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,4 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+
+# approach 1
 # get the occurrences of the elements in nums if count is 1 then append to the result list
-        return [i for i in set(nums) if nums.count(i) == 1]
+        # return [i for i in set(nums) if nums.count(i) == 1]
+
+# approach 2
+# res is set()
+        res = set()
+# for every element in nums
+        for i in nums:
+# if i is in res remove the element
+            if i in res:
+                res.remove(i)
+# if i not in set then add i
+            else:
+                res.add(i)
+# return res 
+        return res
