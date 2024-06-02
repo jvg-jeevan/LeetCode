be6483ad2130ac29a35d022fa5356ec1f08427e2
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-06-02 15:41:54 +0530

    Time: 167 ms (67.33%), Space: 20.9 MB (69.97%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,5 +3,18 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
+# approach 1
+        # s[:] = s[::-1]
 # s[:] is used to modify the list in place instead of craeting a new instance
+
+# approach 2
+# initialize left as 0 and right as last element index
+        left = 0
+        right = len(s) - 1
+# iterate until left index is less than right
+        while left < right:
+# swap the left and right element
+            s[left], s[right] = s[right], s[left]
+# increment left and decrement right
+            left += 1
+            right -= 1
