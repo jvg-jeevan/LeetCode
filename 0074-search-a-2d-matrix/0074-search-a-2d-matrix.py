12dd9701e1c9325ed57eaf1d5bb485372615e766
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-06-02 18:27:56 +0530

    Time: 43 ms (74.32%), Space: 17 MB (96.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,8 +4,28 @@ class Solution:
         # unpack the matrix into 1d list
         for row in matrix:
             arr.extend(row)
-        # if element in arr then return True or else False
-        if target in arr:
-            return True
-        else:
-            return False
+
+
+        # # if element in arr then return True or else False
+        # if target in arr:
+        #     return True
+        # else:
+        #     return False
+
+
+# binary search
+        low, mid, high = 0, 0, len(arr) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return True
+            
+            elif arr[mid] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return False
