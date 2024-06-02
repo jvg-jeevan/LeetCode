f75fbcd28a289d933e96fc176172d4039325db77
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-06-02 22:12:10 +0530

    Time: 165 ms (14.65%), Space: 19.1 MB (44.4%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
@@ -1,10 +1,14 @@
 class Solution:
     def sortArrayByParityII(self, nums: list[int]) -> list[int]:
+# separate the even and odd numbers into separate list
         evens = list(filter(lambda x: x%2 == 0, nums))
         odds = list(filter(lambda x: x%2 != 0, nums))
+# empty the list nums
         nums.clear()
+# zip() takes each element from both evens and odds
         for i, j in zip(evens, odds):
+# add the element to nums first (even index) even number , second (odd index) odd number 
             nums.append(i)
             nums.append(j)
-
+# return the nums list
         return nums
