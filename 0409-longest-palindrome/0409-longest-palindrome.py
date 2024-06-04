af226b301010200dee194daf1731b26a1676f70f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-06-04 22:26:39 +0530

    Time: 38 ms (49.08%), Space: 16.5 MB (80.97%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+# res to store the number of letters in palindrome
+        res = 0
+# letters is set to store the chars to be in palindrome
+        letters = set()
+
+# iterate through each char in s
+        for i in s:
+# if char is already in set letters then remove the letter and increment res by 2 (i.e element is occurring again or 2 times)
+            if i in letters:
+                letters.remove(i)
+                res += 2
+# if element not in letters then add char to letters
+            else:
+                letters.add(i)
+
+# if any element is left in the letters set then increment res by 1 i.e center element can only be one element if not two element 
+        if letters:
+            res += 1
+# return the res
+        return res
