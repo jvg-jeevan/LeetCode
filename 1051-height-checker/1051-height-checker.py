e012d85188b8bef0f0627bf0d862b3daabaec741
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-06-10 17:21:46 +0530

    Time: 42 ms (37.58%), Space: 16.3 MB (97.08%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,10 +1,13 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+# expected is the new sorted array
         expected = sorted(heights)
-        
+# res to store the count of the numbers not equal
         res = 0
+# take 1 element from each element from heights and expected
         for he, ex in zip(heights, expected):
+# if element at position are not equal then increment res 
             if he != ex:
                 res += 1
-            
+# return the result res
         return res
